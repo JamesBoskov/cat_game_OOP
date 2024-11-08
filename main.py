41e0df11f8630426ec53cef26922f0783eb96030
JamesBoskov <james.boskov@laetottenham.ac.uk>
2024-11-08 11:59:32 +0000

    created cat class

---
+++ main.py
@@ -0,0 +1,9 @@
+#cat entity
+class cat:
+    #constructor
+    #creates cat in code
+    def __init__(self, given_name, given_colour):
+        self.name = given_name
+        self.colour = given_colour
+
+mimi = cat("Mimi", "Brown")
